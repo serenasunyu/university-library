de5e5db2fb4569c1cf50b46ed87efbc1891f3b48
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2025-02-18 14:18:21 -0500

    complete home page, including the book info and latest books

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import BookOverview from "@/components/BookOverview";
+import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
+
+const Home = () => (
+    <>
+      <BookOverview {...sampleBooks[0]} />
+
+      <BookList 
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
+    </>
+  );
+
+export default Home;
